c3c076f3d4334c150da61503d78d88c8d152882c
goldhand <will@django.nu>
2016-04-03 19:42:45 -0400

    dont remove readme on existing project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,12 +12,13 @@ is_existing = '{{ cookiecutter.existing_project }}' == 'y'
 
 
 def remove_file(project_directory, file_name):
-    """Removes nginx files"""
+    """Removes a file"""
     file_location = os.path.join(
         project_directory,
         file_name
     )
     os.remove(file_location)
+
 
 def remove_tilda(project_directory, file_name):
     current_location = os.path.join(
@@ -30,6 +31,7 @@ def remove_tilda(project_directory, file_name):
     )
     shutil.move(current_location, new_location)
 
+
 def add_webpack_to_gitignore(project_directory):
 
     file_location = os.path.join(
@@ -37,14 +39,15 @@ def add_webpack_to_gitignore(project_directory):
         '.gitignore'
     )
     with open(file_location, 'a') as f:
-        f.write("\n# Webpack\n%s\n%s\n" % (local_output_path, production_output_path))
+        f.write("\n# Webpack\n%s\n%s\n" % (
+            local_output_path, production_output_path))
 
 
 if is_existing:
-    # remove the webpack gitignore and readme, use existitng project versions instead
+    """
+    remove the webpack gitignore
+    use existitng project versions instead
+    """
     remove_file(PROJECT_DIRECTORY, '.gitignore')
-    remove_file(PROJECT_DIRECTORY, 'README.md')
     remove_tilda(PROJECT_DIRECTORY, '.gitignore')
     add_webpack_to_gitignore(PROJECT_DIRECTORY)
-
-
