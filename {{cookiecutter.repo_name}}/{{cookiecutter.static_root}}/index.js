d996c8190ce3b6d2bca4afb215c5f9950623f4af
Will Farley <willf@hzdg.com>
2016-05-29 12:32:21 -0400

    Remove devtools from production

--- {{cookiecutter.repo_name}}/{{cookiecutter.static_root}}/index.js
+++ {{cookiecutter.repo_name}}/{{cookiecutter.static_root}}/index.js
@@ -4,21 +4,41 @@ import {Router, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
 
 import routes from './routes';
-import DevTools from './containers/DevTools';
 import configureStore from './store/configureStore';
 
 
-const store = configureStore();
+const
+  STORE = configureStore(),
+  ROOT_ELEMENT = 'main';
+
+let ProjectElement;
+
+if (process.env.NODE_ENV !== 'production') {
+
+  // development
+  const DevTools = require('./containers/DevTools').default;
+
+  ProjectElement = (
+    <div>
+      <Router history={browserHistory} routes={routes} />
+      <DevTools />
+    </div>
+  );
+
+} else {
+
+  // production
+  ProjectElement = <Router history={browserHistory} routes={routes} />;
+
+}
 
 // handle client side rendering
 if (typeof document !== 'undefined') {
+
   ReactDOM.render(
-    <Provider store={store}>
-      <div>
-        <Router history={browserHistory} routes={routes} />
-        <DevTools />
-      </div>
+    <Provider store={STORE}>
+      {ProjectElement}
     </Provider>,
-    document.getElementById('main')
+    document.getElementById(ROOT_ELEMENT)
   );
 }
