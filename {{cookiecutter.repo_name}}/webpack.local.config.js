a4a00316a2213e6da16a56267b71daae354d1faf
Will Farley <willf@hzdg.com>
2016-06-07 14:29:46 -0400

    Add eslint loader to local config and ignore test dir

--- {{cookiecutter.repo_name}}/webpack.local.config.js
+++ {{cookiecutter.repo_name}}/webpack.local.config.js
@@ -14,9 +14,12 @@ config.plugins = config.plugins.concat([
   new webpack.NoErrorsPlugin(),{% if cookiecutter.existing_project == 'y' %}
   // local bundle stats file
   new BundleTracker({filename: './webpack-stats.json'}),
-{% endif %}]);
+{% endif %}
+]);
 // local plugins here
 config.module.loaders.push(
+  // lint in our local server
+  {test: /(\.jsx|\.js)$/, loader: 'eslint-loader', exclude: /node_modules/}
 );
 
 module.exports = config;
